12fbe5fa24f2fdfc1ea16d33e5701cc6916b80b0
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-03-12 13:19:45 -0400

    did code and chaned comments

--- main.ts
+++ main.ts
@@ -2,7 +2,23 @@
  *
  * Created by: XXX
  * Created on: Sep 2020
- * This program ...
+ * This program can find the distance utlisiing a sonar. 
 */
 
-basic.showString('Hello, World!')
+//variable
+let distanceObject:number = 0 
+
+// start
+basic.showIcon(IconNames.Happy)
+
+//finds the distance with sonar 
+input.onButtonPressed(Button.A, function(){
+  basic.clearScreen()
+  distanceObject = sonar.ping (
+  DigitalPin.P1,
+  DigitalPin.P2,
+  PingUnit.Centimeters
+)
+  basic.showString((distanceObject) + " cm")
+  basic.showIcon(IconNames.Happy)
+})
