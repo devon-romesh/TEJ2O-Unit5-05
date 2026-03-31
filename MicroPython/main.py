37382643d99d27b41ec39024bb4962705d413dc6
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-03-31 17:01:33 +0000

    coded working version 1.0

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,13 +1,13 @@
 """
 Created by: Devon
 Created on: Mar 2026
-This module is a Micro:bit MicroPython program
+This module is a Micro:bit MicroPython program that lights up NeoPixels based on how much light there is.
 """
 
 from microbit import *
 import neopixel
 
-# variables 
+# variables
 lightLevels = 0
 myNeopixelStrip = neopixel.NeoPixel(pin16, 4)
 
@@ -22,27 +22,26 @@ while True:
         lightLevels = display.read_light_level()
         myNeopixelStrip.clear()
 
-        # if lightLevels is more than 52
+        # if lightLevels is more than 52 but less than 104
         if lightLevels > 52:
-            myNeopixelStrip[0] = (255, 255, 255) 
+            myNeopixelStrip[0] = (255, 255, 255)
 
-        # if lightLevels is more than 104
+        # if lightLevels is more than 104 but less than 156
         if lightLevels > 104:
             myNeopixelStrip[1] = (255, 255, 255)
 
-        # if lightLevels is more than 156
+        # if lightLevels is more than 156 but less than 208
         if lightLevels > 156:
             myNeopixelStrip[2] = (255, 255, 255)
 
-        # if lightLevels is more than 208 
+        # if lightLevels is more than 208
         if lightLevels > 208:
             myNeopixelStrip[3] = (255, 255, 255)
+            myNeopixelStrip.show()
+            display.scroll("Light level is " + (lightLevels))
 
-        myNeopixelStrip.show()
-        display.scroll("Light level is " + (lightLevels))
-
-    if button_b.is_pressed():
-        display.clear()
-        myNeopixelStrip.clear()
-        myNeopixelStrip.show()
-        display.show(Image.HAPPY)
+        if button_b.is_pressed():
+            display.clear()
+            myNeopixelStrip.clear()
+            myNeopixelStrip.show()
+            display.show(Image.HAPPY)
